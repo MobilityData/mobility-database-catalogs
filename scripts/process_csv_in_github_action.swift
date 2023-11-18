2261831e3f15752f17182472827cfd1d2840fee5
Fred <frederic-contractor@mobilitydata.org>
2023-11-18 15:56:01 -0500

    fix: attempt to fix the single quote interference
    Single quotes are used in some names (like McGill's) and this causes an interference in the bash script that uses single quotes to delimit the python script arguments. This should fix it.

--- scripts/process_csv_in_github_action.swift
+++ scripts/process_csv_in_github_action.swift
@@ -203,6 +203,9 @@ if CommandLine.argc == 5 {
 
     } // END FOR LOOP
 
+    // Replace single quotes (like in McGill's) with an apostrophe so there is no interference with the bash script in the next step.
+    PYTHON_SCRIPT_OUTPUT = PYTHON_SCRIPT_OUTPUT.replacingOccurrences(of: "'", with: "ʼ")
+
     // return final output so the action can grab it and pass it on to the Python script.
     print(PYTHON_SCRIPT_OUTPUT.dropFirst())
 

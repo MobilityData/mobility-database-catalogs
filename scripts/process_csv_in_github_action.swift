6383da311bf77c63d6e4bd8668ddd63b41920a8e
Frédéric Simard <frederic-contractor@mobilitydata.org>
2024-02-01 22:02:48 -0500

    Possible fix for missing double quotes. (#326)
    * Fix: updated colums, added debug mode, other fixes
    
    Changed the Google Sheet serving as the source of submissions by providers. Columns needed to change. Added a debug mode. Other minor fixes.
    
    * fix: Changed Google sheet source
    
    The source for the submissions has changed, switched from a Github secret to a 1Password service account path for this URL, added a git command to remove existing and uncommitted files before starting the script.
    
    * Fix: possible fix for double quotes missing in python step

--- scripts/process_csv_in_github_action.swift
+++ scripts/process_csv_in_github_action.swift
@@ -258,9 +258,10 @@ if CommandLine.argc == 5 {
     PYTHON_SCRIPT_OUTPUT = PYTHON_SCRIPT_OUTPUT.replacingOccurrences(of: "'", with: "ʼ")
 
     // Replace double quotes (like "") with a single quote so there is no interference with the bash script in the next step.
-    PYTHON_SCRIPT_OUTPUT = PYTHON_SCRIPT_OUTPUT.replacingOccurrences(of: "\"\",", with: "\",")
-    PYTHON_SCRIPT_OUTPUT = PYTHON_SCRIPT_OUTPUT.replacingOccurrences(of: "=\"\"", with: "=\"")
-    PYTHON_SCRIPT_OUTPUT = PYTHON_SCRIPT_OUTPUT.replacingOccurrences(of: "\"\")", with: "\")")
+
+    // PYTHON_SCRIPT_OUTPUT = PYTHON_SCRIPT_OUTPUT.replacingOccurrences(of: "\"\",", with: "\",")
+    // PYTHON_SCRIPT_OUTPUT = PYTHON_SCRIPT_OUTPUT.replacingOccurrences(of: "=\"\"", with: "=\"")
+    // PYTHON_SCRIPT_OUTPUT = PYTHON_SCRIPT_OUTPUT.replacingOccurrences(of: "\"\")", with: "\")")
 
     // return final output so the action can grab it and pass it on to the Python script.
     print(PYTHON_SCRIPT_OUTPUT.dropFirst())
